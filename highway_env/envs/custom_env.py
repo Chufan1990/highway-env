02196d94b28755d46a8f34f9948f5d2bcdaed832
Chufan Kong <Chufan1990@gmail.com>
2022-04-18 16:15:29 +0800

    Update custom_env.py

--- highway_env/envs/custom_env.py
+++ highway_env/envs/custom_env.py
@@ -1,16 +1,16 @@
+from tabnanny import verbose
 import numpy as np
 from gym.envs.registration import register
 
 from highway_env import utils
-from highway_env.envs.common.abstract import AbstractEnv
+from highway_env.envs.highway_env import HighwayEnvFast
+from highway_env.vehicle.objects import Landmark
 from highway_env.envs.common.action import Action
-from highway_env.road.road import Road, RoadNetwork
-from highway_env.utils import near_split
 from highway_env.vehicle.controller import ControlledVehicle
+from highway_env.vehicle import kinematics
 from highway_env.vehicle.kinematics import Vehicle
 from highway_env.envs.highway_env import HighwayEnvFast
 from highway_env.vehicle.objects import Landmark
-
 
 class CustomHighwayEnvFast(HighwayEnvFast):
     @classmethod
@@ -24,8 +24,8 @@ class CustomHighwayEnvFast(HighwayEnvFast):
             "ego_spacing": 1.5,
             "longitudinal_position_offset": 100,
             "feature_collision_check_time": 1.0,
-            "lane_congest_reward": -0.5,
-            "reward_congest_range": [1, 3]
+            "congest_reward": -0.5,
+            "reward_congest_range": [0, 3],
         })
         return cfg
         
@@ -84,9 +84,34 @@ class CustomHighwayEnvFast(HighwayEnvFast):
                     vehicles_behind += 1
         return vehicles_behind
 
-    @property
-    def vehicles_behind(self):
-        return self._blocked_vehicles
+    def vehicles_behind(self, vehicle: 'kinematics.Vehicle', verbose: int = 0) \
+            -> int:
+        """
+        Find the preceding and following vehicles of a given vehicle.
+
+        :param vehicle: the vehicle whose neighbours must be found
+        :param lane_index: the lane on which to look for preceding and following vehicles.
+                     It doesn't have to be the current vehicle lane but can also be another lane, in which case the
+                     vehicle is projected on it considering its local coordinates in the lane.
+        :return: its preceding vehicle, its following vehicle
+        """
+        v_rear = 0
+        lane_index = vehicle.lane_index
+        if not lane_index:
+            return v_rear
+        lane = self.road.network.get_lane(lane_index)
+        s = self.road.network.get_lane(lane_index).local_coordinates(vehicle.position)[0]
+        for v in self.road.vehicles + self.road.objects:
+            if v is not vehicle and not isinstance(v, Landmark):  # self.network.is_connected_road(v.lane_index,
+                # lane_index, same_lane=True):
+                s_v, lat_v = lane.local_coordinates(v.position)
+                if not lane.on_lane(v.position, s_v, lat_v, margin=1):
+                    continue
+                if s > s_v and abs(vehicle.lane_distance_to(v)) < (self.config["reward_congest_range"][1] + 1)* Vehicle.LENGTH:
+                    v_rear += 1
+                    if verbose > 0:
+                        print(f"blocked vehicles: {v}")
+        return v_rear
 
     def _reward(self, action: Action) -> float:
         """
@@ -100,20 +125,23 @@ class CustomHighwayEnvFast(HighwayEnvFast):
         # Use forward speed rather than speed, see https://github.com/eleurent/highway-env/issues/268
         forward_speed = self.vehicle.speed * np.cos(self.vehicle.heading)
         scaled_speed = utils.lmap(forward_speed, self.config["reward_speed_range"], [0, 1])
-        self._blocked_vehicles = self.congest_vehicles(self.vehicle)
-        scaled_congest_level = utils.lmap(self._blocked_vehicles, self.config["reward_congest_range"], [0, 1])
-        print(f"scaled_congest_level {scaled_congest_level}")
+        self._vehicles_blocked = self.vehicles_behind(self.vehicle)
+        scaled_blocked_vehicles = utils.lmap(self._vehicles_blocked, self.config["reward_congest_range"], [0, 1])
         reward = \
             + self.config["collision_reward"] * self.vehicle.crashed \
             + self.config["right_lane_reward"] * lane / max(len(neighbours) - 1, 1) \
             + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1) \
-            + self.config["lane_congest_reward"] * np.clip(scaled_congest_level, 0, 1)
+            + self.config["congest_reward"] * np.clip(scaled_blocked_vehicles, 0, 1)
         reward = utils.lmap(reward,
-                          [self.config["collision_reward"] + self.config["lane_congest_reward"],
+                          [self.config["collision_reward"] + self.config["congest_reward"],
                            self.config["high_speed_reward"] + self.config["right_lane_reward"]],
                           [0, 1])
         reward = 0 if not self.vehicle.on_road else reward
         return reward
+
+    @property
+    def vehicles_blocked(self) -> int:
+        return self._vehicles_blocked
 
 register(
     id='highway-fast-v1',
